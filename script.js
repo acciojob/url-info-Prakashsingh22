4c666c48bea5e632a1a99feb3f06128ad3f844e0
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-26 19:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+    window.onload = function() {
+      // Step 1: Get the current URL using the location object
+      const currentURL = window.location.href;
+
+      // Step 2: Use the length property to count the number of characters
+      const urlLength = currentURL.length;
+
+      // Step 3: Display the result in the required format
+      alert("The length of the URL is: " + urlLength);
+    };
